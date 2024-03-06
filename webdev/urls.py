d0dcdbb2b4de48b10878c019ffc2ff067d1e7549
Hélio do Nascimento Silva <hedonasi.1979@gmail.com>
2024-03-06 01:50:50 -0300

    Resolução de bugs

--- webdev/urls.py
+++ webdev/urls.py
@@ -6,7 +6,7 @@ The `urlpatterns` list routes URLs to views. For more information please see:
 Examples:
 Function views
     1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+    2. Add a URL to urlpatterns:  path('', views. Home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from webdev.home_view import home
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home),
 ]
